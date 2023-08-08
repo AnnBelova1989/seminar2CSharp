743a0eaf847c093d427ad1b4b98a8703af048ca5
AnnBelova <anutik-89@tut.by>
2023-08-08 20:11:12 +0300

    задача 1 массив

---
+++ primer1/Program.cs
@@ -0,0 +1,9 @@
+﻿int num = new Random().Next(10, 100);
+int firstnum = num/10;
+int secondnum = num % 10;
+int max = firstnum;
+if(secondnum>firstnum)
+{
+max=secondnum;
+}
+System.Console.WriteLine($"наибольшая цифра числа {num} = {max} ");
