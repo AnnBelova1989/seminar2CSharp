e9397af91edab829e6d3db210a9c4df213b32c09
AnnBelova <anutik-89@tut.by>
2023-08-08 21:55:24 +0300

    задача 3

---
+++ primer4/Program.cs
@@ -0,0 +1,14 @@
+﻿System.Console.WriteLine( "Введите первое число: ");
+int firstnumber= int.Parse(Console.ReadLine());
+System.Console.WriteLine( "Введите второе число: ");
+int secondnumber= int.Parse(Console.ReadLine());
+if(firstnumber%secondnumber==0)
+{
+    System.Console.WriteLine($"Число {secondnumber} кратно {firstnumber} ");
+}
+else 
+{
+    int Mod = firstnumber%secondnumber;
+        System.Console.WriteLine($"Число {secondnumber} не кратно {firstnumber} , остаток от деления равен {Mod}");
+}
+
