8e94df44cb498a8fa6b909fa3eef40cbe4c6e092
AnnBelova <anutik-89@tut.by>
2023-08-08 20:43:18 +0300

    задача 1.1

---
+++ primer2/Program.cs
@@ -0,0 +1,14 @@
+﻿int MaxDigit(int num)
+{
+    int firstnum = num / 10;
+    int secondnum = num % 10;
+    int max = firstnum;
+    if (secondnum > firstnum)
+    {
+        max = secondnum;
+    }
+    return max;
+}
+int numb = new Random().Next(10, 100);
+int maxDigit = MaxDigit(numbb);
+System.Console.WriteLine($"наибольшая цифра числа {numb} = {maxDigit} ");
