c862e1b2001daa1cca45610eb92057a563c09e14
AnnBelova <anutik-89@tut.by>
2023-08-08 22:56:25 +0300

    задача 3.1

---
+++ primer5/Program.cs
@@ -0,0 +1,24 @@
+﻿int VvodChisel(string message)
+{
+    System.Console.WriteLine(message);
+    return int.Parse(Console.ReadLine());
+}
+bool Multiplisity(int num1, int num2)
+{
+    if(num1 % num2 == 0)
+    {
+        return true;
+    }
+    return false;
+}
+int firstnum = VvodChisel("Введите первое число: ");
+int secondnum = VvodChisel("Введите второе число: ");
+bool result = Multiplisity(firstnum, secondnum);
+if(result == true)
+{
+    System.Console.WriteLine($"Число {secondnum} кратно {firstnum} ");
+}
+else
+{
+    System.Console.WriteLine($"Число {secondnum} не кратно {firstnum} , остаток от деления равен {firstnum % secondnum}");
+}
