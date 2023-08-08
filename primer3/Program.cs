0ac197095cc1f1dc3bd9c4096dfa1e4796e4a4ba
AnnBelova <anutik-89@tut.by>
2023-08-08 21:18:17 +0300

    задача 2

---
+++ primer3/Program.cs
@@ -0,0 +1,8 @@
+﻿int Twonum(int num)
+{
+    return num/100*10+num%10;
+}
+int num = new Random().Next(100,1000);
+int twonum= Twonum(num);
+System.Console.WriteLine($"трехзначное число {num} ");
+System.Console.WriteLine($"число без второй цифры {twonum}");
